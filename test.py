251cb04d54be47253875152a9e8c788264982f8b
alecbetancourt <alecbetancourt@gmail.com>
2025-02-01 18:38:53 -0500

    Iniial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import rdDistGeom
+from rdkit.Chem import rdMolAlign
+from rdkit.Chem import rdMolDescriptors
+from rdkit.Chem import rdMolTransforms
+from rdkit.Chem import rdForceFieldHelpers
+
+from rdkit.Chem import PyMol
+import copy
+import numpy as np
