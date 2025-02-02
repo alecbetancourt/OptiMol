ac4bc2b42916ec7a5ccd9639a31c8371196d8a35
Caitlynn Curley <curleyc@umich.edu>
2025-02-01 19:53:53 -0500

    Add MD simulation

--- test.py
+++ test.py
@@ -9,3 +9,21 @@ from rdkit.Chem import rdForceFieldHelpers
 from rdkit.Chem import PyMol
 import copy
 import numpy as np
+
+from openmm.app import *
+from openmm import *
+from openmm.unit import *
+from sys import stdout
+
+inpcrd = AmberInpcrdFile('diala.inpcrd')
+prmtop = AmberPrmtopFile('diala.prmtop', periodicBoxVectors=inpcrd.boxVectors)
+system = prmtop.createSystem(nonbondedMethod=NoCutoff, nonbondedCutoff=1*nanometer,
+        constraints=HBonds)
+integrator = VerletIntegrator(0.001*picoseconds)
+simulation = Simulation(prmtop.topology, system, integrator)
+simulation.context.setPositions(inpcrd.positions)
+simulation.minimizeEnergy()
+simulation.reporters.append(PDBReporter('output.pdb', 1000))
+simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
+        potentialEnergy=True, temperature=True))
+simulation.step(10000)
