f747fc59942ae1cfbf8c21b05b712a19d3a8d983
Ryo Ikeda <ryoikeda@umich.edu>
2025-02-01 21:18:42 -0500

    Added restraint

--- test.py
+++ test.py
@@ -19,6 +19,9 @@ inpcrd = AmberInpcrdFile('diala.inpcrd')
 prmtop = AmberPrmtopFile('diala.prmtop', periodicBoxVectors=inpcrd.boxVectors)
 system = prmtop.createSystem(nonbondedMethod=NoCutoff, nonbondedCutoff=1*nanometer,
         constraints=HBonds)
+restraint = PeriodicTorsionForce()
+system.addForce(restraint)
+restraint.addTorsion(t1, t2, t3, t4, 1, restraintAngle * radians, 100*kilojoules_per_mole) 
 integrator = VerletIntegrator(0.001*picoseconds)
 simulation = Simulation(prmtop.topology, system, integrator)
 simulation.context.setPositions(inpcrd.positions)
@@ -27,3 +30,14 @@ simulation.reporters.append(PDBReporter('output.pdb', 1000))
 simulation.reporters.append(StateDataReporter(stdout, 1000, step=True,
         potentialEnergy=True, temperature=True))
 simulation.step(10000)
+
+for bond in rotatableBonds:
+    for i in range(36):
+        addRestraint()
+        runSimulation()
+        conformerList.append(simulationResult)
+finalConformations = filterResults(conformerList)
+
+restraintEnergy = (restraint_angle-actual_angle)*force_constant
+restraintEnergy = absolute_value((30-100)*100) = 0
+restraintEnergy = absolute_value((30-100)*100) = 7000
